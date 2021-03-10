7dd2d925c3ea3038cc21b19fccbd3d2b86966205
ukorall <ukorall@gmail.com>
2021-03-10 13:36:47 +0200

    symbols moved from tables to linkedList

--- linkedList.h
+++ linkedList.h
@@ -13,3 +13,18 @@ void addToList(struct lnode *list, Word *value);
 
 Word getFromList(struct lnode *list, int index);
 
+typedef struct symbols{
+    Int12 address;
+    char *name;
+    char *attributes;
+    struct symbols *next;
+    int value;
+} Symbols;
+
+Symbols *Slistalloc();
+
+void addToTable(struct symbols *newS, char *name, char *attributes, int val, int lineNumber);
+
+void setVal(struct symbols *s, int val);
+
+void setAddress(struct symbols *s, char *name, Int12 address); /*for the second run*/
--- tables.h
+++ tables.h
@@ -1,5 +1,3 @@
-#include "dataModel.h"
-
 #ifndef TABLES_INCLUDED
 #define TABLES_INCLUDED
 
@@ -22,20 +20,6 @@ struct label /*entries and externals tables*/
     int value;
     char attributes[4]; /*code/entry/data/external*
 };*/
-typedef struct symbols{
-    Int12 address;
-    char *name;
-    char *attributes;
-    struct symbols *next;
-    int value;
-} Symbols;
 
-Symbols *Slistalloc();
-
-void addToTable(struct symbols *newS, char *name, char *attributes, int val, int lineNumber);
-
-void setVal(struct symbols *s, int val);
-
-void setAddress(struct symbols *s, char *name, Int12 address); /*for the second run*/
 
 #endif
