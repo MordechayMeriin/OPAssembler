1ee26c14bfa6537a3388878fd43f4b0fd1d33aae
ukorall <ukorall@gmail.com>
2021-03-11 15:23:22 +0200

    possible basic structre for secondRun in a comment

--- secondRun.c
+++ secondRun.c
@@ -29,3 +29,56 @@ void second(List *codeList, List *dataList, Symbols *SymbolList /*, char **label
     }
     /*build output files*/
 }
+
+/*void second(FILE *file, List *codeList, List *dataList, Symbols *SymbolList)
+{
+    extern int IC, DC, ICF, DCF, L;
+    int lineNumber = 1;
+    char label[MAXWORD];
+    char *firstWord = NULL;
+    char *line = (char *)calloc(sizeof(char), MAXLINE);
+    Symbols *tmp;
+
+    if (line == NULL)
+    {
+        mallocError("string");
+    }
+
+    while(fgets(line, MAXLINE, file) != NULL)
+    {
+        deleteBlanks(lineNumber, line);
+        if(!isEmpty(line))
+        {
+            line = getWord(line, firstWord);
+            if(isItLable(lineNumber, firstWord))
+            {
+                line = getWord(line, firstWord);
+            }
+            if(isItDir(firstWord))
+            {
+                if (strcmp(firstWord, ".entry")==0)
+                {
+                    for(tmp=SymbolList ; strcmp(tmp->name, line)!=0 && tmp!=NULL ; tmp=tmp->next)
+                    ;
+                    if(strcmp(labelName, tmp->name)==0)
+                        strcat(tmp->attributes, ", entry");
+                    else
+                        errorLog(lineNumber, strcat(line, " is not exists"));
+                }
+            }
+            else
+            {
+                **complete command coding
+            }
+        }
+        lineNumber++;
+    }
+    if(areErrorsExist())
+    {
+        printErrors();
+    }
+    else
+    {
+        files creating
+    }
+}*/
