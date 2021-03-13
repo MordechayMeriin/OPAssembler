93fd0b7898691acfa3a7de4ddaf4bdf90692a6ef
ukorall <ukorall@gmail.com>
2021-03-13 21:14:44 +0200

    addOperand2

--- secondRun.c
+++ secondRun.c
@@ -4,11 +4,14 @@
 
 void second(FILE *file, List *codeList, List *dataList, Symbols *SymbolList)
 {
-    extern int IC, DC, ICF, DCF, L;
-    int lineNumber = 1;
+    extern int IC, DC, ICF, DCF;
+    int L, lineNumber = 1;
     char *firstWord = NULL;
     char *line = (char *)calloc(sizeof(char), MAXLINE);
     Symbols *tmp;
+    Rule *rule;
+    char **operands;
+    OpWord *operation;
 
     if (line == NULL)
     {
@@ -34,12 +37,38 @@ void second(FILE *file, List *codeList, List *dataList, Symbols *SymbolList)
                     if(strcmp(line, tmp->name)==0)
                         strcat(tmp->attributes, ", entry");
                     else
-                        errorLog(lineNumber, strcat(line, " is not exists"));
+                        errorLog(lineNumber, strcat(line, " is not a known label"));
                 }
             }
             else
             {
                 /**complete command coding*/ 
+                operation = (OpWord *)calloc(sizeof(OpWord), 1);
+                if (operation == NULL)
+                {
+                    mallocError("OpWord");
+                }
+                L = 1;
+                rule = getRule(firstWord);
+                operands = getOperands(line, lineNumber);
+                
+                operation->opcode = rule->opcode;
+                operation->funct = rule->funct;
+
+                if (operands[0] != NULL)
+                {
+                    L++;
+                    if (operands[1] != NULL)
+                    {
+                        L++;
+                        addOperand2(operation, SymbolList, rule, operands[0], SOURCE_OPERAND, lineNumber);
+                        addOperand2(operation, SymbolList, rule, operands[1], TARGET_OPERAND, lineNumber);
+                    }
+                    else
+                    {
+                        addOperand2(operation, SymbolList, rule, operands[0], TARGET_OPERAND, lineNumber);
+                    }
+                }
             }
         }
         lineNumber++;
@@ -51,5 +80,43 @@ void second(FILE *file, List *codeList, List *dataList, Symbols *SymbolList)
     else
     {
         /*files creating*/
+    }
+}
+
+void addOperand2(OpWord *operation, Symbols SymbolList, Rule *rule, char *operand, int operandType, int lineNumber)
+{
+    int rel=0;
+    Symbols *tmp;
+    if (*operand != '#' && !isRegister(operand))
+    {
+        if (*operand == '%')
+        {
+            operand++;
+            rel=1;
+        }
+        if(validLabel(operand))
+        {
+            for(; strcmp(SymbolList->name, operand)!=0 && SymbolList!=NULL ; SymbolList=SymbolList->next)
+                ;
+            if(strcmp(operand, tmp->name)==0)
+            {
+                if(rel) /*Relative Addressing*/
+                {
+                    ;
+                }
+                else /*Direct addressing*/
+                {
+                    ;
+                }
+            }
+            else
+            {
+                errorLog(strcat(operand, " is not a known label"), lineNumber);
+            }
+        }
+        else
+        {
+            errorLog(strcat(operand, " is not a valid label"), lineNumber);
+        }
     }
 }
